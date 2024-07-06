51cb656df69492fb7e6ec8960d126981defc85ee
AzamatUzakov <azamatakmal053@gmail.com>
2024-07-06 19:10:22 +0500

    next

--- main.js
+++ main.js
@@ -1,332 +1,50 @@
-import './style.css'
 
-document.querySelector('#app').innerHTML = `
-  <div class="content">
-			<div class="container">
-				<div class="tab tab_product">
-					<div class="catalog catalog_item">
-						<div class="item">
-							<div class="item_image"></div>
-							<div class="item_data">
-								<p class="item_title">Фарфоровая фигурка "Лисица кицуне"</p>
-								<div class="item_meta">
-									<div class="item_prices">
-										<p class="price">2 399 &#8381;</p>
-										<p class="old_price">2 500 &#8381;</p>
-									</div>
-									<div class="item_stock">
-										<span>3</span> шт
-									</div>
-								</div>
-								<div class="item_description">
-									Наше дело не так однозначно, как может показаться: социально-экономическое развитие предполагает независимые способы реализации первоочередных требований. С учётом сложившейся международной обстановки, постоянный количественный рост и сфера нашей активности однозначно определяет каждого участника как способного принимать собственные решения касаемо позиций, занимаемых участниками в отношении поставленных задач! Наше дело не так однозначно, как может показаться: социально-экономическое развитие предполагает независимые способы реализации первоочередных требований. С учётом сложившейся международной обстановки, постоянный количественный рост и сфера нашей активности однозначно определяет каждого участника как способного принимать собственные решения касаемо позиций, занимаемых участниками в отношении поставленных задач! 
-								</div>
-							</div>
-							<a href="javascript:void(0);" class="item_gradient">
-								<span>Развернуть</span>
-							</a>
-						</div>					
-					</div>
-				</div>
-				<div class="tab tab_main tab_active">
-					<div class="catalog">
-						<a class="item openProduct">
-							<div class="item_image"></div>
-							<div class="item_data">
-								<div class="item_meta">
-									<div class="item_prices">
-										<p class="price">2 399 &#8381;</p>
-										<p class="old_price">2 500 &#8381;</p>
-									</div>
-									<div class="item_stock">
-										<span>3</span> шт
-									</div>
-								</div>
-								<p class="item_title">Фарфоровая фигурка "Лисица кицуне"</p>
-								<button type="button" class="btn addToCart">Добавить в корзину</button>
-							</div>
-						</a>
-						<a class="item openProduct">
-							<div class="item_image"></div>
-							<div class="item_data">
-								<div class="item_meta">
-									<div class="item_prices">
-										<p class="price">2 399 &#8381;</p>
-										<p class="old_price">2 500 &#8381;</p>
-									</div>
-									<div class="item_stock">
-										<span>3</span> шт
-									</div>
-								</div>
-								<p class="item_title">Фарфоровая фигурка "Лисица кицуне"</p>
-								<button type="button" class="btn addToCart">Добавить в корзину</button>
-							</div>
-						</a>
-						<a class="item openProduct">
-							<div class="item_image"></div>
-							<div class="item_data">
-								<div class="item_meta">
-									<div class="item_prices">
-										<p class="price">2 399 &#8381;</p>
-										<p class="old_price">2 500 &#8381;</p>
-									</div>
-									<div class="item_stock">
-										<span>3</span> шт
-									</div>
-								</div>
-								<p class="item_title">Фарфоровая фигурка "Лисица кицуне"</p>
-								<button type="button" class="btn addToCart">Добавить в корзину</button>
-							</div>
-						</a>
-						<a class="item openProduct">
-							<div class="item_image"></div>
-							<div class="item_data">
-								<div class="item_meta">
-									<div class="item_prices">
-										<p class="price">2 399 &#8381;</p>
-										<p class="old_price">2 500 &#8381;</p>
-									</div>
-									<div class="item_stock">
-										<span>3</span> шт
-									</div>
-								</div>
-								<p class="item_title">Фарфоровая фигурка "Лисица кицуне"</p>
-								<button type="button" class="btn addToCart">Добавить в корзину</button>
-							</div>
-						</a>
-						<a class="item openProduct">
-							<div class="item_image"></div>
-							<div class="item_data">
-								<div class="item_meta">
-									<div class="item_prices">
-										<p class="price">2 399 &#8381;</p>
-										<p class="old_price">2 500 &#8381;</p>
-									</div>
-									<div class="item_stock">
-										<span>3</span> шт
-									</div>
-								</div>
-								<p class="item_title">Фарфоровая фигурка "Лисица кицуне"</p>
-								<button type="button" class="btn addToCart">Добавить в корзину</button>
-							</div>
-						</a>
-						<a class="item openProduct">
-							<div class="item_image"></div>
-							<div class="item_data">
-								<div class="item_meta">
-									<div class="item_prices">
-										<p class="price">2 399 &#8381;</p>
-										<p class="old_price">2 500 &#8381;</p>
-									</div>
-									<div class="item_stock">
-										<span>3</span> шт
-									</div>
-								</div>
-								<p class="item_title">Фарфоровая фигурка "Лисица кицуне"</p>
-								<button type="button" class="btn addToCart">Добавить в корзину</button>
-							</div>
-						</a>
-						<a class="item openProduct">
-							<div class="item_image"></div>
-							<div class="item_data">
-								<div class="item_meta">
-									<div class="item_prices">
-										<p class="price">2 399 &#8381;</p>
-										<p class="old_price">2 500 &#8381;</p>
-									</div>
-									<div class="item_stock">
-										<span>3</span> шт
-									</div>
-								</div>
-								<p class="item_title">Фарфоровая фигурка "Лисица кицуне"</p>
-								<button type="button" class="btn addToCart">Добавить в корзину</button>
-							</div>
-						</a>
-						<a class="item openProduct">
-							<div class="item_image"></div>
-							<div class="item_data">
-								<div class="item_meta">
-									<div class="item_prices">
-										<p class="price">2 399 &#8381;</p>
-										<p class="old_price">2 500 &#8381;</p>
-									</div>
-									<div class="item_stock">
-										<span>3</span> шт
-									</div>
-								</div>
-								<p class="item_title">Фарфоровая фигурка "Лисица кицуне"</p>
-								<button type="button" class="btn addToCart">Добавить в корзину</button>
-							</div>
-						</a>
-					</div>
-				</div>
-				<div class="tab tab_orders">
-					<div class="tab_title">
-						<p>Ваши заказы</p>
-					</div>
-					<div class="catalog catalog_rows">
-						<div class="item">
-							<div class="item_image"></div>
-							<div class="item_data">
-								<p class="item_title">Фарфоровая фигурка "Лисица кицуне"</p>
-								<p class="item_p">Заказ: ААА000</p>
-								<p class="item_p item_status">
-									Статус:
-									<span class="badge badge_green">доставлен</span>
-								</p>
-								<p class="item_p">Создан: 6 Июня в 12:10</p>
-								<a href="#" class="btn btn_inline">отменить заказ</a>
-							</div>							
-						</div>
-						<div class="item">
-							<div class="item_image"></div>
-							<div class="item_data">
-								<p class="item_title">Фарфоровая фигурка "Лисица кицуне"</p>
-								<p class="item_p">Заказ: ААА000</p>
-								<p class="item_p item_status">
-									Статус:
-									<span class="badge badge_blue">в пути</span>
-								</p>
-								<p class="item_p">Создан: 6 Июня в 12:10</p>
-								<a href="#" class="btn btn_inline">отменить заказ</a>
-							</div>							
-						</div>
-						<div class="item">
-							<div class="item_image"></div>
-							<div class="item_data">
-								<p class="item_title">Фарфоровая фигурка "Лисица кицуне"</p>
-								<p class="item_p">Заказ: ААА000</p>
-								<p class="item_p item_status">
-									Статус:
-									<span class="badge badge_brown">собирается</span>
-								</p>
-								<p class="item_p">Создан: 6 Июня в 12:10</p>
-								<a href="#" class="btn btn_inline">отменить заказ</a>
-							</div>							
-						</div>
-						<div class="item">
-							<div class="item_image"></div>
-							<div class="item_data">
-								<p class="item_title">Фарфоровая фигурка "Лисица кицуне"</p>
-								<p class="item_p">Заказ: ААА000</p>
-								<p class="item_p item_status">
-									Статус:
-									<span class="badge badge_green">доставлен</span>
-								</p>
-								<p class="item_p">Создан: 6 Июня в 12:10</p>
-								<a href="#" class="btn btn_inline">отменить заказ</a>
-							</div>							
-						</div>						
-					</div>
-				</div>
-				<div class="tab tab_cart">
-					<div class="tab_title">
-						<p>Корзина</p>
-					</div>
-					<div class="catalog catalog_cart catalog_rows">
-						<div class="item">
-							<div class="item_image"></div>
-							<div class="item_data">
-								<p class="item_title">Фарфоровая фигурка "Лисица кицуне"</p>
-								<div class="item_meta">
-									<div class="item_prices">
-										<p class="price">2 399 &#8381;</p>
-										<p class="old_price">2 500 &#8381;</p>
-									</div>
-								</div>
-								<div class="item_controls">
-									<div class="item_q">
-										<button class="item_qm">-</button>
-										<input type="text" value="1" />
-										<button class="item_qp">+</button>
-									</div>
-								</div>
-								<a href="#" class="btn btn_inline">удалить</a>
-							</div>							
-						</div>
-						<div class="item">
-							<div class="item_image"></div>
-							<div class="item_data">
-								<p class="item_title">Фарфоровая фигурка "Лисица кицуне"</p>
-								<div class="item_meta">
-									<div class="item_prices">
-										<p class="price">2 399 &#8381;</p>
-										<p class="old_price">2 500 &#8381;</p>
-									</div>
-								</div>
-								<div class="item_controls">
-									<div class="item_q">
-										<button class="item_qm">-</button>
-										<input type="text" value="1" />
-										<button class="item_qp">+</button>
-									</div>
-								</div>
-								<a href="#" class="btn btn_inline">удалить</a>
-							</div>							
-						</div>	
-						<div class="item">
-							<div class="item_image"></div>
-							<div class="item_data">
-								<p class="item_title">Фарфоровая фигурка "Лисица кицуне"</p>
-								<div class="item_meta">
-									<div class="item_prices">
-										<p class="price">2 399 &#8381;</p>
-										<p class="old_price">2 500 &#8381;</p>
-									</div>
-								</div>
-								<div class="item_controls">
-									<div class="item_q">
-										<button class="item_qm">-</button>
-										<input type="text" value="1" />
-										<button class="item_qp">+</button>
-									</div>
-								</div>
-								<a href="#" class="btn btn_inline">удалить</a>
-							</div>							
-						</div>
-						<div class="item">
-							<div class="item_image"></div>
-							<div class="item_data">
-								<p class="item_title">Фарфоровая фигурка "Лисица кицуне"</p>
-								<div class="item_meta">
-									<div class="item_prices">
-										<p class="price">2 399 &#8381;</p>
-										<p class="old_price">2 500 &#8381;</p>
-									</div>
-								</div>
-								<div class="item_controls">
-									<div class="item_q">
-										<button class="item_qm">-</button>
-										<input type="text" value="1" />
-										<button class="item_qp">+</button>
-									</div>
-								</div>
-								<a href="#" class="btn btn_inline">удалить</a>
-							</div>							
-						</div>						
-					</div>
-				</div>
-			</div>
-		</div>
-		<nav>
-			<div class="contaienr">
-				<ul>
-					<li class="active">
-						<a href="javascript:void(0);" data-tab="main" class="openTab">
-							<i class="icon icon_shop"></i>
-						</a>
-					</li>
-					<li>
-						<a href="javascript:void(0);" data-tab="orders" class="openTab">
-							<i class="icon icon_orders"></i>
-						</a>
-					</li>
-					<li>
-						<a href="javascript:void(0);" data-tab="cart" class="openTab">
-							<i class="icon icon_cart"></i>
-						</a>
-					</li>
-				</ul>
-			</div>
-		</nav>
-`
+let tg = window.Telegram.WebApp;
+tg.expand();
+tg.MainButton.hide();
+
+
+
+jQuery(function($) {
+$(document).ready(function() {
+
+$('.catalog_item').css('height', $(window).height() - 90);
+
+$(document).on('click', '.openTab', function() {
+    
+    var thisTab = $(this).data('tab');
+    var thisTabObject = $('.tab.tab_' + thisTab);
+    if (!thisTabObject) {
+        return false;
+    }
+    
+    $('.tab').removeClass('tab_active')
+    thisTabObject.addClass('tab_active');
+    
+    $('ul').find('li').removeClass('active');
+    $(this).parent().addClass('active');
+    
+    return false;
+    
+});
+
+$(document).on('click', '.openProduct', function() {
+    
+    $('.tab').removeClass('tab_active');
+    $('.tab_product').addClass('tab_active');
+    
+    return false;
+    
+});
+
+$(document).on('click', '.item_gradient', function() {
+        
+    $(this).parent().parent().addClass('open');
+    $(this).remove();
+    
+    return false;
+    
+});
+
+});
+});
