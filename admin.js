132e9610056110335737aa4e75f07470bfa2520c
alexsxc <alexanderqwsrtuz@gmail.com>
2024-07-20 20:36:40 +0300

    fix

--- admin.js
+++ admin.js
@@ -1,48 +1,48 @@
 let tg = window.Telegram.WebApp;
 tg.expand();
-tg.MainButton.hide();
 
 jQuery(function($) {
     $(document).ready(function() {
-
         $('.catalog_item').css('height', $(window).height() - 90);
 
         $(document).on('click', '.openTab', function() {
-
             var thisTab = $(this).data('tab');
             var thisTabObject = $('.tab.tab_' + thisTab);
             if (!thisTabObject) {
                 return false;
             }
-
-            $('.tab').removeClass('tab_active')
+            $('.tab').removeClass('tab_active');
             thisTabObject.addClass('tab_active');
-
             $('ul').find('li').removeClass('active');
             $(this).parent().addClass('active');
-
             return false;
-
         });
 
         $(document).on('click', '.openProduct', function() {
-
             $('.tab').removeClass('tab_active');
             $('.tab_product').addClass('tab_active');
-
             return false;
-
         });
 
         $(document).on('click', '.item_gradient', function() {
-
             $(this).parent().parent().addClass('open');
             $(this).remove();
-
             return false;
-
         });
 
+        // Новый код jQuery для обработки кликов по элементам .item
+        $(document).on('click', '.item_length .item', function() {
+            $('.tab_orders').hide();
+            $('.orders_open_window').show();
+        });
+
+        // Обработка клика по кнопке back___btn
+        $(document).on('click', '.back___btn', function() {
+            $('.orders_open_window').hide();
+            $('.tab_orders').show();
+            $('.fixed_done_manipulator_order').hide();
+            return false;
+        });
     });
 });
 
